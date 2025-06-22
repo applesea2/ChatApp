3ea3fade1211450e9fde10eb06498d5b97e4b6c8
Jesse Stroster <stroster.jesse@gmail.com>
2025-06-22 15:17:36 -0400

    Improved login ui and added redirect to chat

--- chatapp.client/src/app/login/login.component.ts
+++ chatapp.client/src/app/login/login.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   standalone: false,
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   username = '';
   password = '';
   error = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   login() {
     this.http.post<{ token: string }>('https://localhost:7219/api/login/login', {
@@ -21,9 +23,11 @@ export class LoginComponent {
       next: (res) => {
         localStorage.setItem('jwtToken', res.token);
         this.error = '';
-        // Optionally, redirect or update UI
+        // Redirect to chat component on successful login
+        this.router.navigate(['/chat']);
       },
       error: () => {
+        // Display error message on unsuccessful login
         this.error = 'Invalid username or password';
       }
     });
